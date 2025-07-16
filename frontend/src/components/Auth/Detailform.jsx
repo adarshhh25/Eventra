c56e0d53b7d6cdca0a0f2428ee19c21d84c6ddd3
adarshhh25 <adarshjha4425@gmail.com>
2025-07-16 20:52:27 +0530

    Added a form for signup page

--- frontend/src/components/Auth/Detailform.jsx
+++ frontend/src/components/Auth/Detailform.jsx
@@ -5,19 +5,32 @@ import { useParams } from 'react-router-dom';
 function Detailform(props) {
   const { role } = useParams(); 
   return (
-    <div>
+    <div className='Details-form'>
       <div className="detail-form">
-            <h1>{role === "candidate" ? (<p>Sign up as candidate</p>) : (<p>Sign up as host</p>)}</h1>
-            <label htmlFor="firstName">First Name<span className="required">*</span></label>
-              <input
-                type="text"
-                id="firstName"
-                name="firstName"
-                placeholder="First Name"
-              />
+        <h1>{role === "candidate" ? "Sign up as candidate" : "Sign up as host"}</h1>
+        <form className='form'>
+          <div className="name-details">
+            <label htmlFor="firstName">First Name</label>
+            <input id="firstName" required placeholder='First Name'/>
+            <label htmlFor="lastName">Last Name</label>
+            <input id="lastName" required placeholder='Last Name'/>
+          </div>
+          <input type='email' placeholder={role === "candidate" ? "Email" : "Offical Email"}required />
+          <input type='number' required />
+          <div className="gender">
+            <button>Male</button>
+            <button>Female</button>
+          </div>
+          <div className="passwords">
+            <input type='password'/>
+            <input type='password'/>
+          </div>
+        </form>
+           <button>Next</button>
       </div>
     </div>
   )
 }
+
 
 export default Detailform
