614b101d0b1323d3d3077f6e011d67e6230484b3
adarshhh25 <adarshjha4425@gmail.com>
2025-07-17 20:44:57 +0530

    Changes Done

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import connectDB from "./db.js"
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+const MONGO_URL = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/eventra";
 
 app.listen(PORT, () => {
     console.log(`Server running on PORT : ${PORT}`)
 })
+
+connectDB();
--- frontend/src/components/Auth/Detailform.jsx
+++ frontend/src/components/Auth/Detailform.jsx
@@ -10,18 +10,24 @@ function Detailform(props) {
       <div className="detail-form">
         <h1>{role === "candidate" ? "Sign up as candidate" : "Sign up as host"}</h1>
         <form className='form'>
-            <label htmlFor="firstName">First Name</label>
-            <input id="firstName" required placeholder='First Name'/>
-            <label htmlFor="lastName">Last Name</label>
-            <input id="lastName" required placeholder='Last Name'/>
-            <label htmlFor="email">Email</label>
-            <input htmlFor="email" type='email' placeholder={role === "candidate" ? "Email" : "Offical Email"}required />
+            <div class="name-details">
+                <div>
+                  <label htmlFor="name">Name:</label><br />
+                  <input id="name" required/>
+                </div>
+                <div>
+                  <label htmlFor="last-name">Last Name:</label><br />
+                  <input id="last-name" required/>
+                </div>
+              </div>
+            <lable htmlFor="email">Email</lable>
+            <input id="email" type='email' className="small-input" placeholder={role === "candidate" ? "Email" : "Offical Email"}required />
             <label htmlFor="phone">Phone</label>
-            <input htmlFor="phone" type='number' required />
+            <input htmlFor="phone" type='number'className='small-input' required />
                <div className="gender">
                   <span>Gender</span>
-                  <button>Male</button>
-                  <button>Female</button>
+                  <button onClick={(e) => {e.preventDefault()}}>Male</button>
+                  <button onClick={(e) => {e.preventDefault()}}>Female</button>
                </div>
           <div className="passwords">
             <label htmlFor="password">Password</label>
